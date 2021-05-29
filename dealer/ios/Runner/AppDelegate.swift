e191300c464338379668e17dfd5a1d967cf92a44
unknown <titoyanac@gmail.com>
2021-05-28 19:24:11 -0500

    sin api key

--- dealer/ios/Runner/AppDelegate.swift
+++ dealer/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAtR_sk1XrM-PiU-eEXP_Yo9uky8JY8ff4")
+    GMSServices.provideAPIKey("[TU API KEY AQUI]")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
